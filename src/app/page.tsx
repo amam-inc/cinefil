dc6e7603b308021037c2ade18752f95cf96bb074
Matis Byar <matis.byar.06@icloud.com>
2024-10-08 22:17:07 +0200

    small styles adjustments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function HomePage({
   const query = searchParams?.query ?? "";
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] p-4 text-white">
+    <main className="flex min-h-screen flex-col items-center justify-center p-4 ">
       <Search placeholder={"Search movies..."} />
 
       <Suspense key={query} fallback={<div>Loading...</div>}>
