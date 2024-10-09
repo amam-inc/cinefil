db02071540ee38c8f859bce42c4d7f5d14660ef1
Maxime Bodin <maxime.bodin@mailo.com>
2024-10-09 10:49:36 +0200

    tmdb.ts : Adding getMovieDetails.

--- src/server/services/tmdb.ts
+++ src/server/services/tmdb.ts
@@ -1,5 +1,25 @@
 'use server'
-import {Movie, Search, TMDB} from 'tmdb-ts';
+import {
+    AlternativeTitles,
+    Changes,
+    Credits,
+    ExternalIds,
+    Images,
+    Keywords,
+    Movie,
+    MovieChangeValue,
+    MovieDetails,
+    MovieLists,
+    Recommendations,
+    ReleaseDates,
+    Reviews,
+    Search,
+    SimilarMovies,
+    TMDB,
+    Translations,
+    Videos,
+    WatchProviders
+} from 'tmdb-ts';
 import {RateLimiterMemory} from 'rate-limiter-flexible';
 
 const tmdb: TMDB = new TMDB(process.env.TMDB_API_TOKEN as string);
@@ -31,5 +51,40 @@ export const searchMovies = async (query: string): Promise<Search<Movie>> => {
         // Handle TMDB API errors.
         console.error('Error fetching movies:', err);
         throw new Error('Failed to fetch movies from TMDB.');
+    }
+};
+
+
+/**
+ * TMDB Service to get details for a given movie.
+ * @param id
+ */
+export const getMovieDetails = async (id: number): Promise<(MovieDetails & { credits: Omit<Credits, "id"> } & {
+    videos: Omit<Videos, "id">
+} & { images: Omit<Images, "id"> } & { recommendations: Recommendations } & { reviews: Omit<Reviews, "id"> } & {
+    reviews: Omit<Translations, "id">
+} & { changes: Changes<MovieChangeValue> } & { keywords: Omit<Keywords, "id"> } & { lists: Omit<MovieLists, "id"> } & {
+    release_dates: Omit<ReleaseDates, "id">
+} & { alternative_titles: Omit<AlternativeTitles, "id"> } & { external_ids: Omit<ExternalIds, "id"> } & {
+    translations: Omit<Translations, "id">
+} & { "watch/providers": Omit<WatchProviders, "id"> } & object & {
+    similar: SimilarMovies
+}) | (MovieDetails & object)> => {
+    // Use rate limiter to prevent excessive calls.
+    try {
+        await rateLimiter.consume(id);
+
+        // Will throw an error if rate limit exceeded.
+    } catch (rateLimitError) {
+        throw new Error('Too many requests, please try again later.');
+    }
+
+    try {
+        // Call the TMDB API.
+        return await tmdb.movies.details(id);
+    } catch (err) {
+        // Handle TMDB API errors.
+        console.error('Error fetching movies:', err);
+        throw new Error('Failed to get details for movie from TMDB.');
     }
 };
