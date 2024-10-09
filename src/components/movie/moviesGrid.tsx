9ff9bb05900e8befd548f88b6ad0de839fe40844
Maxime Bodin <maxime.bodin@mailo.com>
2024-10-09 11:19:26 +0200

    moviesGrid.tsx : Update movies grid style.

--- src/components/movie/moviesGrid.tsx
+++ src/components/movie/moviesGrid.tsx
@@ -10,7 +10,7 @@ export default async function MoviesGrid({query, filmId}: { query: string, filmI
     return (
         <div
             className={`grid gap-6 w-full m-10 ${
-                filmId ? 'grid-cols-3 lg:grid-cols-2' : 'grid-cols-2 sm:grid-cols-4 lg:grid-cols-6'
+                filmId ? 'lg:grid-cols-3 grid-cols-1 sm:grid-cols-2' : 'grid-cols-2 sm:grid-cols-4 lg:grid-cols-6'
             }`}>
             {movies.results.map((movie: Movie) => (
                 <MoviesCard movie={movie} key={movie.id}/>
