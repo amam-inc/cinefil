37420e987f42894b8198aba244fba416e4efaa44
Maxime Bodin <maxime.bodin@mailo.com>
2024-10-09 11:08:16 +0200

    layout.tsx : Adding ThemeProvider.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,31 @@
 import "@/styles/globals.css";
 
-import { GeistSans } from "geist/font/sans";
-import { type Metadata } from "next";
+import {GeistSans} from "geist/font/sans";
+import {type Metadata} from "next";
 import React from "react";
+import {ThemeProvider} from "@/components/themeProvider";
 
 export const metadata: Metadata = {
-  title: "CinéFIL",
-  description: "Site de votre cinéma CinéFIL à IMT Atlantique",
-  icons: [{ rel: "icon", url: "/favicon.ico" }],
+    title: "CinéFIL",
+    description: "Site de votre cinéma CinéFIL à IMT Atlantique",
+    icons: [{rel: "icon", url: "/favicon.ico"}],
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{ children: React.ReactNode }>) {
-  return (
-    <html lang="fr" className={`${GeistSans.variable}`}>
-      <body>{children}</body>
-    </html>
-  );
+                                       children,
+                                   }: Readonly<{ children: React.ReactNode }>) {
+    return (
+        <html lang="fr" className={`${GeistSans.variable}`}>
+        <body>
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+        >
+            {children}
+        </ThemeProvider>
+        </body>
+        </html>
+    );
 }
