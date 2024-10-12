9bfc9199859d0c68b2fef2f52626406454d465b8
Matis Byar <matis.byar.06@icloud.com>
2024-10-12 15:09:37 +0200

    update: improved visual aesthetics with Avatar, sign out button

--- src/components/accountDropdown.tsx
+++ src/components/accountDropdown.tsx
@@ -1,6 +1,8 @@
-import { signOut } from "@/app/login/actions";
+import { signOut } from "@/app/(unprotected)/login/actions";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { createClient } from "@/utils/supabase/server";
+import { type User } from "@supabase/auth-js";
 import Link from "next/link";
 
 /**
@@ -11,16 +13,18 @@ import Link from "next/link";
 export default async function AccountDropdown() {
     const supabase = createClient()
     
-    const {data, error} = await supabase.auth.getUser()
+    const {data: {user}} = await supabase.auth.getUser() as { data: { user: User | null } };
     
-    return (
-        <div className="flex items-center gap-4">
-            <div>{ data ? <b>{ data?.user?.user_metadata.name }</b> : null }</div>
-            { data?.user ? (
+    return user ? (
+            <div className="flex items-center gap-4">
+                <div className="flex items-center gap-2">
+                    <Avatar>
+                        <AvatarImage src={ `${ user.user_metadata.avatar_url }` }/>
+                        <AvatarFallback>?</AvatarFallback> </Avatar>
+                    <b>{ user?.user_metadata.name }</b>
+                </div>
                 <form><Button variant="outline" formAction={ signOut }>Sign out</Button></form>
-            ) : (
-                <Link href={ "/login" }><Button variant="outline">Sign in</Button></Link>
-            ) }
-        </div>
-    );
+            </div>
+        ) :
+        <Link href={ "/login" }><Button variant="outline">Sign in</Button></Link>
 }
