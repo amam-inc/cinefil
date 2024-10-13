ed21086fd9bc433663b27ad6832fd14fcf922265
Matis Byar <matis.byar.06@icloud.com>
2024-10-13 14:26:53 +0200

    update: fixed lint errors

--- src/components/suggestions.tsx
+++ src/components/suggestions.tsx
@@ -1,14 +1,14 @@
-import {createClient} from "../../utils/supabase/server";
-import React, {Suspense} from "react";
-import {type Suggestion} from "@/types/suggestion";
-import {getMovieDetails} from "@/server/services/tmdb";
 import MoviesGrid from "@/components/movie/moviesGrid";
-import {type Movie} from "tmdb-ts";
+import { getMovieDetails } from "@/server/services/tmdb";
+import { type Suggestion } from "@/types/suggestion";
+import { createClient } from "@/utils/supabase/server";
+import React, { Suspense } from "react";
+import { type Movie } from "tmdb-ts";
 
 export default async function Suggestions({displayShown}: {
     displayShown?: string | undefined,
 }) {
-
+    
     /**
      * Fetch movie details for each suggestion based on tmdb_id.
      * @param suggestions
@@ -26,8 +26,8 @@ export default async function Suggestions({displayShown}: {
      */
     const fetchSuggestions = async () => {
         const {data, error} = await createClient().from("suggestions").select()
-            .order('shownOn', { ascending: false });  // Sort by shownOn in descending order.
-
+            .order('shownOn', {ascending: false});  // Sort by shownOn in descending order.
+        
         if (error) {
             console.error("Error fetching suggestions:", error);
             // TODO : Add error handlingg.
@@ -43,8 +43,8 @@ export default async function Suggestions({displayShown}: {
         <div className="flex flex-col">
             <h1 className="text-2xl font-bold text-center mt-4">Suggestions des utilisateurs</h1>
             <Suspense>
-                <MoviesGrid movies={await fetchSuggestions() as unknown[] as Movie[]} forSuggestions={true}
-                            displayShown={displayShown}/>
+                <MoviesGrid movies={ await fetchSuggestions() as unknown[] as Movie[] } forSuggestions={ true }
+                            displayShown={ displayShown }/>
             </Suspense>
         </div>
     );
